63b9d2247be7392d4b3f100bed6248314a342fc5
dmd <dmd@feip.co>
2017-05-18 23:03:15 +1000

    Small fix

--- app/Test.php
+++ app/Test.php
@@ -3,9 +3,11 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-
+use TCG\Voyager\Models\Category;
 
 class Test extends Model
 {
-    
+    public function categoryId(){
+        return $this->belongsTo(Category::class);
+    }
 }
